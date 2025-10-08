3cc25d79b604064d94c2293e5a12e2cedf6345ca
SivasliPatron <ebubekir.kayaoez@hotmail.com>
2025-10-08 03:43:57 +0200

    Fix build.sh - Remove FFmpeg installation (already installed on Render)

--- build.sh
+++ build.sh
@@ -6,9 +6,5 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Install FFmpeg (required for audio conversion)
-apt-get update
-apt-get install -y ffmpeg
-
 # Create downloads directory
 mkdir -p downloads
